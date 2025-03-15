2293c9287a7b87356057df9abcaf92574ac738aa
leosalgado <leonardosalgado@protonmail.com>
2025-03-15 14:56:04 -0300

    Create dynamic properties

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
 export class BigCardComponent {
-
+  @Input()
+  imgCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDesc: string = '';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
-
+  @Input()
+  imgCover: string = '';
+  @Input()
+  cardTitle: string = '';
 }
